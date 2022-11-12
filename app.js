dfae1f7b6f61d3fbb527d9f0a91da2ae4a1723ff
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2022-11-12 23:52:16 +0300

    Separated handlers from the route and futher use the route method to make the code much more better

--- app.js
+++ app.js
@@ -88,14 +88,21 @@ const deleteTour = (req, res) => {
 
 
 //Routes and there functions calling the route handlers
-app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours', getAllTours)
 app.get('/api/v1/tours/:id', getTour)
-app.post('/api/v1/tours', addTour)
+// app.post('/api/v1/tours', addTour)
 app.patch('/api/v1/tours/:id', updateTour)
 app.delete('/api/v1/tours/:id', deleteTour)
 
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(addTour)
 
-
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour).patch(updateTour)
+  .delete(deleteTour)
 
 
 //start a server
